4cc80f0bf7b894f3fb27acaad470cb1bc49260d4
gmsabbirahmed1 <148498744+gmsabbirahmed1@users.noreply.github.com>
2024-06-22 03:16:34 +0600

    Update STRING INPUT .java

--- STRING INPUT .java
+++ STRING INPUT .java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {   Scanner sc=new Scanner(System.in);
+        System.out.print("What is your name:");
         String s = sc.nextLine();
-        System.out.println(s);
+        System.out.println("Hello, "+s+ "! Have a good day.");
     }
 }
