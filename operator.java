f085f5027c3753b1c2b42eec0443024b3f1c3240
gmsabbirahmed1 <148498744+gmsabbirahmed1@users.noreply.github.com>
2024-06-24 02:36:37 +0600

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,10 @@
+package com.comphany;
+
+public class CWH_ch2_operators {
+    public static void main(String[] args)
+    {   int a = 8;
+        int b = 10 + a;
+        System.out.println(b);
+
+    }
+}
