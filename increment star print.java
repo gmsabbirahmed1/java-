260a737020ee3191405755e8df8a60f5dfa01e3a
gmsabbirahmed1 <148498744+gmsabbirahmed1@users.noreply.github.com>
2024-09-16 00:54:01 +0600

    Create increment star.java

---
+++ increment star print.java
@@ -0,0 +1,18 @@
+package FirstPackage;
+
+public class loop {
+    public static void main(String[] args) {
+        int i,j,n=4;
+        for(i=1; i<=n; i++){
+            for (j=i; j>0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
+
